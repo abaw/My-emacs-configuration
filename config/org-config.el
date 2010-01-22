ab0414f0c43e3c4d917b482b0f1d90b4f2c40bef
ken <ken.wu@tainet.net>
2010-01-22 18:15:05 +0800

    1. added org-6.34c

--- config/org-config.el
+++ config/org-config.el
@@ -1,6 +1,9 @@
-(when (fboundp 'org-mode)
-  (setq org-todo-keywords
-	'((sequence "TODO" "WAIT" "|" "DONE" "DELEGATED" "CANCELED")))
-  (add-hook 'org-mode-hook
-	    #'(lambda ()
-		(flyspell-mode t))))
+(add-to-list 'load-path (concat my-emacs-lisp-dir (file-name-as-directory "org-mode") "lisp"))
+(add-to-list 'load-path (concat my-emacs-lisp-dir (file-name-as-directory "org-mode") (file-name-as-directory "contrib") "lisp"))
+
+(abaw-try-to-require 'org-install)
+(setq org-todo-keywords
+      '((sequence "TODO" "WAIT" "|" "DONE" "DELEGATED" "CANCELED")))
+(add-hook 'org-mode-hook
+	  #'(lambda ()
+	      (flyspell-mode t)))
