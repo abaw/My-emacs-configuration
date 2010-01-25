4e4ea0042d3957d959970b382b70e2ed8cf330f4
ken <ken.wu@tainet.net>
2010-01-25 18:11:24 +0800

    1. load xscope support

--- config/basic-config.el
+++ config/basic-config.el
@@ -38,7 +38,7 @@
 
 ;; load various useful packages
 (mapc  #'(lambda (package) (abaw-try-to-require package))
-       `(highlight-parentheses dired+ dired-sort-menu hide-region hide-lines eol-conversion))
+       `(highlight-parentheses dired+ dired-sort-menu hide-region hide-lines eol-conversion xscope))
 
 ;; common hot keys
 (global-set-key (kbd "C-c r") 'font-lock-fontify-buffer)
