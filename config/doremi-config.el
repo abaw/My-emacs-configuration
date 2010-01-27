bd91206623af5cc853c916f8d52a80c2dad89eb0
Ken Wu <abawwu@gmail.com>
2010-01-27 10:00:10 +0800

    1. changed the key binding force interactively moving between windows

--- config/doremi-config.el
+++ config/doremi-config.el
@@ -2,18 +2,7 @@
 (defmacro doremi-key (key)
   `(kbd ,(concat "C-x t " key)))
 
-;; FIXME: there are a lot of duplicated codes
-(defun windmove-interactively (key)
-  "interactively move between windows"
-  (interactive "kUsing arrow keys to move cursor between windows")
-  (cond
-   ((equal key [left]) (windmove-left) (call-interactively 'windmove-interactively))
-   ((equal key [right]) (windmove-right) (call-interactively 'windmove-interactively))
-   ((equal key [up]) (windmove-up) (call-interactively 'windmove-interactively))
-   ((equal key [down]) (windmove-down) (call-interactively 'windmove-interactively))))
+(when (abaw-try-to-require 'windmove)
+  (windmove-default-keybindings 'control))
 
 (global-set-key (doremi-key "r") 'doremi-window-width)
-(global-set-key (doremi-key "<up>") 'windmove-up)
-(global-set-key (doremi-key "<down>") 'windmove-down)
-(global-set-key (doremi-key "<left>") 'windmove-left)
-(global-set-key (doremi-key "<right>") 'windmove-right)
