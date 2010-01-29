6b8bd11062a016632ae4296ebc1bde7d804c8e58
Ken Wu <abawwu@gmail.com>
2010-01-29 11:47:51 +0800

    1. for elisp buffer, using M-. and M-, for finding functions.

--- config/lisp-config.el
+++ config/lisp-config.el
@@ -1,6 +1,9 @@
 ;; lisp hook
 (defun my/lisp-hook ()
-  (eldoc-mode t))
+  (eldoc-mode t)
+  (when (eq major-mode 'emacs-lisp-mode)
+      (local-set-key (kbd "M-.") 'find-function)
+      (local-set-key (kbd "M-,") 'pop-global-mark)))
 
 (add-hook 'lisp-mode-hook 'my/lisp-hook)
 (add-hook 'emacs-lisp-mode-hook 'my/lisp-hook)
