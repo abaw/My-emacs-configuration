7596b1ef7143c1101fe677d4184d18a4449741ea
Ken Wu <abawwu@gmail.com>
2010-02-02 15:26:36 +0800

    1. changed hot key to pop mark in elisp buffers

--- config/lisp-config.el
+++ config/lisp-config.el
@@ -3,7 +3,7 @@
   (eldoc-mode t)
   (when (eq major-mode 'emacs-lisp-mode)
       (local-set-key (kbd "M-.") 'find-function)
-      (local-set-key (kbd "M-,") 'pop-global-mark)))
+      (local-set-key (kbd "M-*") 'pop-global-mark)))
 
 (add-hook 'lisp-mode-hook 'my/lisp-hook)
 (add-hook 'emacs-lisp-mode-hook 'my/lisp-hook)
