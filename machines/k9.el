073f6d9443a4ff0e8158547d070dfec382c2801b
Ken Wu <abawwu@gmail.com>
2010-02-03 18:38:29 +0800

    1. set some custom variables on k9

--- machines/k9.el
+++ machines/k9.el
@@ -36,3 +36,16 @@
 (load "my-anything-config")
 
 (put 'set-goal-column 'disabled nil)
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(flyspell-use-meta-tab nil)
+ '(yas/trigger-key "<C-tab>"))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
