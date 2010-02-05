422c5d208e0060328d40b6de585e2618cebe35df
Ken Wu <abawwu@gmail.com>
2010-02-05 17:35:44 +0800

    1. used a more general way to unbind keys we don't want

--- config/unbind-key-config.el
+++ config/unbind-key-config.el
@@ -1,6 +1,15 @@
 ;; unbind keys we don't want to have
 
-(add-hook 'comint-mode-hook
-	  (lambda ()
-	    (define-key comint-mode-map (kbd "C-<up>") nil)
-	    (define-key comint-mode-map (kbd "C-<down>") nil)))
+(defvar my-keys-may-conflict (list (kbd "C-<up>") (kbd "C-<down>"))
+  "The keys that may conflicts my hot key settings in some major
+  modes")
+
+(defvar my-major-modes-with-key-conflict (list 'comint-mode 'eshell-mode))
+
+(defun my-unbind-conflict-keys ()
+  (dolist (key my-keys-may-conflict)
+    (local-set-key key nil)))
+
+(dolist (mode my-major-modes-with-key-conflict)
+  (let ((hook-symbol (intern (concat (symbol-name mode) "-hook"))))
+    (add-hook hook-symbol 'my-unbind-conflict-keys)))
