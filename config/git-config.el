ad9b9e7a74f9e96c194ff17a06218fbe30bd5550
Ken Wu <abawwu@gmail.com>
2010-02-05 15:09:08 +0800

    1. better anything-c-source-git-project-files

--- config/git-config.el
+++ config/git-config.el
@@ -10,15 +10,33 @@
     (compilation-start grep-command 'grep-mode))
 
   (when (abaw-try-to-require 'anything-config)
+    (defvar anything-c-source-git-project-files-cache nil "(path signature cached-buffer)")
     (defvar anything-c-source-git-project-files
       '((name . "Files from Current GIT Project")
-	(init . (lambda () (setq anything-git-top-dir (magit-get-top-dir (if (buffer-file-name)
-									     (file-name-directory (buffer-file-name))
-									   default-directory)))))
-        (candidates . (lambda ()
-			(if anything-git-top-dir
-			    (let ((default-directory anything-git-top-dir))
-			      (mapcar (lambda (file) (concat default-directory file)) (magit-shell-lines (magit-format-git-command "ls-files" nil)))))))
-        (type . file)))
-    (add-to-list 'anything-for-files-prefered-list 'anything-c-source-git-project-files)))
+	(init . (lambda ()
+		  (let* ((top-dir (magit-get-top-dir (if (buffer-file-name)
+							 (file-name-directory (buffer-file-name))
+						       default-directory)))
+			 (default-directory top-dir)
+			 (signature (magit-shell (magit-format-git-command "rev-parse --verify HEAD" nil))))
 
+		    (unless (and anything-c-source-git-project-files-cache
+				 (third anything-c-source-git-project-files-cache)
+				 (equal (first anything-c-source-git-project-files-cache) top-dir)
+				 (equal (second anything-c-source-git-project-files-cache) signature))
+		      (if (third anything-c-source-git-project-files-cache)
+			  (kill-buffer (third anything-c-source-git-project-files-cache)))
+		      (setq anything-c-source-git-project-files-cache
+			    (list top-dir
+				  signature
+				  (anything-candidate-buffer 'local)))
+		      (with-current-buffer (third anything-c-source-git-project-files-cache)
+			(dolist (filename (mapcar (lambda (file) (concat default-directory file))
+						  (magit-shell-lines (magit-format-git-command "ls-files" nil))))
+			  (insert filename)
+			  (newline))))
+		    (anything-candidate-buffer (third anything-c-source-git-project-files-cache)))))
+
+        (type . file)
+	(candidates-in-buffer)))
+    (add-to-list 'anything-for-files-prefered-list 'anything-c-source-git-project-files)))
