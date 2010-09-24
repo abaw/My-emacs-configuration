816f3132be87eb80c19f32c0b66a91e3725e4d82
Ken Wu <abawwu@gmail.com>
2010-09-24 11:05:03 +0800

    1. using cc mode 5.32 for qt support

--- config/cc-config.el
+++ config/cc-config.el
@@ -1,3 +1,4 @@
+(add-to-list 'load-path (concat my-emacs-lisp-dir (file-name-as-directory "cc-mode")))
 (when (abaw-try-to-require 'ctypes)
   (ctypes-auto-parse-mode 1))
 
@@ -17,7 +18,7 @@
   (when (featurep 'highlight-parentheses) (highlight-parentheses-mode 1)))
 
 ;; for QT
-(setq qt-decl-keywords (list "Q_OBJECT" "Q_CLASSINFO" "Q_PROPERTY" "Q_ENUMS" ))
+;; (setq qt-decl-keywords (list "Q_OBJECT" "Q_CLASSINFO" "Q_PROPERTY" "Q_ENUMS" ))
 ;; TODO: font-lock for QT
 ;; (make-face 'qt-keywords-face)
 ;; (set-face-foreground 'qt-keywords-face "BlueViolet")
@@ -25,14 +26,12 @@
 ;;   (font-lock-add-keywords 'c++-mode (list (concat "\\<" word "\\>")) 'qt-keywords-face))
 
 (defun qt-c++-hook ()
-  (when (functionp 'abaw-at-vsemi-for-decl-macro-p)
-    (setq c-at-vsemi-p-fn 'abaw-at-vsemi-for-decl-macro-p)
-    (setq abaw-class-decl-macro-list qt-decl-keywords)))
+  (add-to-list 'c-macro-names-with-semicolon "Q_CLASSINFO")
+  (c-make-macro-with-semi-re))
 
 (add-hook 'c-mode-hook 'my-c/c++-hook)
 (add-hook 'c++-mode-hook 'my-c/c++-hook)
 (add-hook 'c++-mode-hook 'qt-c++-hook)
-
 
 ;;; styles
 (c-add-style "OCS"
@@ -47,4 +46,3 @@
 		(cpp-macro . 0))))
 (c-add-style "UBOOT"
 	     '("OCS"))
-
