db1b354b13929aeeed17df89af1b517f24cbc1af
Ken Wu <abawwu@gmail.com>
2010-10-07 15:26:43 +0800

    1. setq ack-executable 2. C-M-o binds to anything-c-moccur-buffer-list

--- config/ack-config.el
+++ config/ack-config.el
@@ -1,9 +1,9 @@
-(when (abaw-try-torequire 'full-ack)
+(when (abaw-try-to-require 'full-ack)
+  (setq ack-executable (executable-find "ack-grep"))
   (defun ack-find-all-files (pattern &optional regexp directory more-args)
     (interactive (append (ack-interactive)
 			 (list (split-string (read-from-minibuffer "More arguments(e.g. --cc --cpp):")))))
     (dolist (file (apply 'ack-list-files default-directory
 			 (append more-args (unless (zerop (length pattern)) (list pattern)))))
-      ;; (find-file-noselect file)
-      (message "will find-file %s" file)
+      (find-file-noselect file)
       )))
--- config/my-anything-config.el
+++ config/my-anything-config.el
@@ -7,7 +7,7 @@
 	  anything-c-moccur-enable-initial-pattern t
 	  anything-c-moccur-higligt-info-line-flag)
     (global-set-key (kbd "M-o") 'anything-c-moccur-occur-by-moccur)
-    (global-set-key (kbd "C-M-o") 'anything-c-moccur-dmoccur)
+    (global-set-key (kbd "C-M-o") 'anything-c-moccur-buffer-list)
     (add-hook 'dired-mode-hook
 	      '(lambda ()
 		 (local-set-key (kbd "O") 'anything-c-moccur-dired-do-moccur-by-moccur)))))
