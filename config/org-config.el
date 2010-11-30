409572187cf66ab3bda4af6336d5fb9210248f93
abaw <abawwu@gmail.com>
2010-11-30 23:06:11 +0800

    1. updated org-mode 2. added org-s5

--- config/org-config.el
+++ config/org-config.el
@@ -1,9 +1,14 @@
 (add-to-list 'load-path (concat my-emacs-lisp-dir (file-name-as-directory "org-mode") "lisp"))
 (add-to-list 'load-path (concat my-emacs-lisp-dir (file-name-as-directory "org-mode") (file-name-as-directory "contrib") "lisp"))
 
-(abaw-try-to-require 'org-install)
-(setq org-todo-keywords
-      '((sequence "TODO" "WAIT" "|" "DONE" "DELEGATED" "CANCELED")))
-(add-hook 'org-mode-hook
-	  #'(lambda ()
-	      (flyspell-mode t)))
+(when (abaw-try-to-require 'org-install)
+  (setq org-todo-keywords
+	'((sequence "TODO" "WAIT" "|" "DONE" "DELEGATED" "CANCELED")))
+  (add-hook 'org-mode-hook
+	    #'(lambda ()
+		(flyspell-mode t)))
+
+  (add-hook 'org-babel-after-execute-hook 'org-display-inline-images)
+  (org-babel-do-load-languages 'org-babel-load-languages
+			       '((emacs-lisp . t)
+				 (ditaa . t))))
