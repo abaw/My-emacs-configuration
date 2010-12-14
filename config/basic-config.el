0dde7c837217df5b0c14ea5bd00f56a5a45eab3f
Ken Wu <abawwu@gmail.com>
2010-12-14 16:52:46 +0800

    1. enable paredit in minibuffer for eval-express

--- config/basic-config.el
+++ config/basic-config.el
@@ -149,4 +149,8 @@
 (when (abaw-try-to-require 'paredit)
   (add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
   (add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
-  (add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1))))
+  (add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
+  (add-hook 'minibuffer-setup-hook (lambda ()
+				     (if (eq this-command 'eval-expression)
+					 (paredit-mode 1)))))
+
--- config/lisp-config.el
+++ config/lisp-config.el
@@ -2,8 +2,9 @@
 (defun my/lisp-hook ()
   (eldoc-mode t)
   (when (eq major-mode 'emacs-lisp-mode)
-      (local-set-key (kbd "M-.") 'find-function)
-      (local-set-key (kbd "M-*") 'pop-global-mark)))
+    (local-set-key (kbd "M-.") 'find-function)
+    (local-set-key (kbd "M-*") 'pop-global-mark)))
 
 (add-hook 'lisp-mode-hook 'my/lisp-hook)
 (add-hook 'emacs-lisp-mode-hook 'my/lisp-hook)
+
