8d272e9d439c205947d5a891baab71f05fcd6347
Ken Wu <abawwu@gmail.com>
2010-12-20 10:16:56 +0800

    1. removed slime, I decided to use quicklisp version of slime outside my emacs configuration directory later.

--- config/slime-config.el
+++ config/slime-config.el
@@ -1,7 +1,7 @@
-(add-to-list 'load-path (concat my-emacs-lisp-dir "slime"))
-(setq inferior-lisp-program "sbcl")
-(require 'slime)
-(slime-setup '(slime-fancy))
+;; (add-to-list 'load-path (concat my-emacs-lisp-dir "slime"))
+;; (setq inferior-lisp-program "sbcl")
+;; (require 'slime)
+;; (slime-setup '(slime-fancy))
 
-(when (abaw-try-to-require 'paredit)
-  (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1))))
+;; (when (abaw-try-to-require 'paredit)
+;;   (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1))))
