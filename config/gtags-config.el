8c33bd48b6f20b2a4f10f4336df5affea410e885
abaw <abawwu@gmail.com>
2011-05-10 13:15:55 +0800

    1. configure M-. key to use anything-gtags-select when a prefix argument was given.

--- config/gtags-config.el
+++ config/gtags-config.el
@@ -5,6 +5,15 @@
   (add-hook 'c++-mode-hook 'my-enable-gtags-mode)
 
   (when (abaw-try-to-require 'anything-gtags)
+    (defun my-gtags-find (&optional use-anything)
+      "the convenient command to choose gatgs-find-tag or
+      anything-gtags-select. with the prefix argument,
+      anything-gtags-select will be used."
+      (interactive "P")
+      (if use-anything
+	  (call-interactively 'anything-gtags-select)
+	(call-interactively 'gtags-find-tag)))
+
     (setq anything-gtags-enable-initial-pattern t)
-    (define-key gtags-mode-map (kbd "M-.") 'anything-gtags-select)))
+    (define-key gtags-mode-map (kbd "M-.") 'my-gtags-find)))
 
