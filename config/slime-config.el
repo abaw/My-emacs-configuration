9288421cce65e5c114f12f4f46db6b5ae4265e44
abaw <abawwu@gmail.com>
2011-06-03 10:26:02 +0800

    1. using sbcl with slime

--- config/slime-config.el
+++ config/slime-config.el
@@ -1,8 +1,9 @@
 ;; (add-to-list 'load-path (concat my-emacs-lisp-dir "slime"))
-;; (setq inferior-lisp-program "sbcl")
+
 ;; (require 'slime)
 ;; (slime-setup '(slime-fancy))
 
 ;; (when (abaw-try-to-require 'paredit)
 ;;   (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1))))
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
+(setq inferior-lisp-program "sbcl")
