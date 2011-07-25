13f7f2eec03eadf24ff5d573a1d94b2bb6e83fbf
Ken Wu <ken.wu@ruckuswireless.com>
2011-07-25 11:30:32 +0800

    1. (require 'org) instaed of (require 'org-install) in org-config.el

--- config/org-config.el
+++ config/org-config.el
@@ -1,7 +1,7 @@
 (add-to-list 'load-path (concat my-emacs-lisp-dir (file-name-as-directory "org-mode") "lisp"))
 (add-to-list 'load-path (concat my-emacs-lisp-dir (file-name-as-directory "org-mode") (file-name-as-directory "contrib") "lisp"))
 
-(when (abaw-try-to-require 'org-install)
+(when (abaw-try-to-require 'org)
   (setq org-todo-keywords
 	'((sequence "TODO" "WAIT" "|" "DONE" "DELEGATED" "CANCELED")))
   (add-hook 'org-mode-hook
