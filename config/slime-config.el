ce2bb3539c8d196fcd8dcbc5b2922efc1ecaaaec
Ken Wu <ken.wu@ruckuswireless.com>
2011-08-22 11:31:29 +0800

    1. load slime-helper.el only when it exists

--- config/slime-config.el
+++ config/slime-config.el
@@ -5,6 +5,11 @@
 
 ;; (when (abaw-try-to-require 'paredit)
 ;;   (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1))))
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
-(setq inferior-lisp-program "sbcl")
-(setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)
+
+(let ((helper-file (expand-file-name "~/quicklisp/slime-helper.e")))
+  (if (file-exists-p helper-file)
+      (progn (load helper-file)
+	     (setq inferior-lisp-program "sbcl")
+	     (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol))
+    (message "%s: not found %s" (buffer-file-name) helper-file)))
+
