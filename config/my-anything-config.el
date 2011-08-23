60f9297453920972f5ddb353185d5ba4c639af56
Ken Wu <ken.wu@ruckuswireless.com>
2011-08-23 18:07:14 +0800

    1. tweak anything-for-files configuration 2. bind <f5> to anything-for-files. it is a key binding that will cause less conflict

--- config/my-anything-config.el
+++ config/my-anything-config.el
@@ -1,5 +1,19 @@
 (when (abaw-try-to-require 'anything-config)
-  (global-set-key (kbd "C-c C-f") 'anything-for-files)
+  (global-set-key (kbd "<f5>") 'anything-for-files)
+
+  (defvar anything-c-source-open-files
+    '((name . "Opened Files")
+      (candidates . (lambda () (remove nil (mapcar 'buffer-file-name (buffer-list)))))
+      (action . '("Switch to buffer" . find-file))
+      (action-transformer . (anything-c-transform-file-load-el anything-c-transform-file-browse-url))
+      (candidate-transformer . (anything-c-w32-pathname-transformer anything-c-skip-boring-files anything-c-shorten-home-path))))
+
+  (setq anything-for-files-prefered-list '(anything-c-source-files-in-current-dir+
+					   anything-c-source-buffers+
+					   anything-c-source-open-files
+					   anything-c-source-ffap-guesser
+					   anything-c-source-ffap-line
+					   anything-c-source-recentf))
 
   (when (abaw-try-to-require 'anything-c-moccur)
     (setq moccur-split-word t
