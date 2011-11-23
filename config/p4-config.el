24350868653ef5f085ebbda9a86adb9a04995900
Ken Wu <ken.wu@ruckuswireless.com>
2011-11-23 16:41:18 +0800

    1. alawys use .p4config

--- config/p4-config.el
+++ config/p4-config.el
@@ -2,5 +2,6 @@
 ;; (setq vc-p4-require-p4config t)
 
 ;; use p4.el
-(when (abaw-require 'p4)
- (setq p4-follow-symlinks t))
+(when (abaw-try-to-require 'p4)
+ (setq p4-follow-symlinks t)
+ (setq p4-use-p4config-exclusively t))
