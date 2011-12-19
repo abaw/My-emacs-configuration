c3aefd9f116efdd1b893e5c342d20ebc9b5835c7
Ken Wu <ken.wu@ruckuswireless.com>
2011-12-19 17:02:00 +0800

    1. bind org-agenda to "C-c a"

--- config/org-config.el
+++ config/org-config.el
@@ -2,6 +2,7 @@
 (add-to-list 'load-path (concat my-emacs-lisp-dir (file-name-as-directory "org-mode") (file-name-as-directory "contrib") "lisp"))
 
 (when (abaw-try-to-require 'org)
+  (global-set-key (kbd "C-c a") 'org-agenda)
   (setq org-todo-keywords
 	'((sequence "TODO" "WAIT" "|" "DONE" "DELEGATED" "CANCELED")))
   (add-hook 'org-mode-hook
